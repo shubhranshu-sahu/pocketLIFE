361fc676b9cefc4650dd01be1bdacced1dfd5926
Ashish Umrao <ashishumrao632005@gmail.com>
2025-05-09 14:45:19 +0530

    heatmap be looking good

--- frontend/src/components/AuthButton.jsx
+++ frontend/src/components/AuthButton.jsx
@@ -5,11 +5,20 @@ import { useCookies } from "react-cookie";
 import { Menu } from 'primereact/menu';
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { Dialog } from 'primereact/dialog';
+import { Toast } from 'primereact/toast';
 
 function AuthButton() {
     const [cookies, setCookies, removeCookie] = useCookies(["access_token"]);
     const navigate = useNavigate();
     const menuRight = useRef(null);
+    const toast = useRef(null);
+
+    const showError = () => {
+        toast.current.show({severity:'error', summary: 'Error', detail:'Login Failed', life: 3000});
+    }
+
     const items = [
       {
           label: 'Options',
@@ -21,17 +30,17 @@ function AuthButton() {
                 command(event) {
                     event.originalEvent.type === 'click' && removeCookie("access_token")
                     window.location.reload();
+                  },
                 },
-              },
-          ]
-      }
+              ]
+            }
   ];
 
     const login = useGoogleLogin({
       onSuccess: tokenResponse => {
         console.log(tokenResponse)
+        // show(); 
         axios.get(`http://localhost:3000/auth?code=${tokenResponse['access_token']}`).then((res)=>{
-          console.log(res.data);
           setCookies('access_token', res.data.token);
           localStorage.setItem('username', res.data.user.name)
           localStorage.setItem('email', res.data.user.email)
@@ -39,19 +48,20 @@ function AuthButton() {
           navigate('/')
         }).catch((err)=>{
           console.error(err);
+          showError()
         })
       },
     });
   
+
     const loggedIN = <div>
       <img onClick={(event) => menuRight.current.toggle(event)} aria-controls="popup_menu_right" aria-haspopup style={{borderRadius:'100%'}} width={50} height={50} src={localStorage.getItem('image') || ''} alt="" />
       <Menu model={items} popup ref={menuRight} id="popup_menu_right" popupAlignment="right" />
-      {/* <Button label="Show Right" icon="pi pi-align-right" className="mr-2" onClick={(event) => menuRight.current.toggle(event)} aria-controls="popup_menu_right" aria-haspopup /> */}
-      {/* <button onClick={()=>removeCookie('access_token')}>Log Out</button> */}
     </div>
     return (
       <div>
-        {cookies.access_token ? loggedIN : <Button severity='secondary' icon='pi pi-sign-in' outlined onClick={()=>login()} label='Login/Enter'/> }
+        <Toast ref={toast} />
+        {cookies.access_token ? loggedIN : <Button severity='secondary' icon='pi pi-sign-in' outlined onClick={()=>{login()}} label='Login/Enter'/> }
     </div>
     )
 }
--- frontend/src/components/Heatmap.jsx
+++ frontend/src/components/Heatmap.jsx
@@ -1,21 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CalendarHeatmap from 'react-calendar-heatmap';
 import '../heatmap.css'
 import { Tooltip } from 'react-tooltip'
-
+// import { ResponsiveCalendar } from '@nivo/calendar'
 
 function Heatmap() {
-    const boxes = Array.from(Array(365));
+    // const boxes = Array.from(Array(365));
+    const [dates, setDates] = useState([]);
+    
+
+    useEffect(()=>{
+      var boxes = [];
+      var day = new Date('2025-01-00');
+      for(let i=0; i<=366; i++){
+        var nextDay = new Date(day);
+        nextDay.setDate(day.getDate() + 1);
+        boxes.push({"date": nextDay.toISOString().split('T')[0], "count": Math.round(Math.random()*4)})
+        day = nextDay;
+      }
+
+      setDates(boxes);
+      console.log(boxes)
+    },[])
+
+    const MyResponsiveCalendar = ({ data }) => (
+        <ResponsiveCalendar
+            data={data}
+            from="2025-03-01"
+            to="2025-07-12"
+            emptyColor="#eeeeee"
+            colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+            margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+            yearSpacing={40}
+            monthBorderColor="#ffffff"
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'row',
+                    translateY: 36,
+                    itemCount: 4,
+                    itemWidth: 42,
+                    itemHeight: 36,
+                    itemsSpacing: 14,
+                    itemDirection: 'right-to-left'
+                }
+            ]}
+        />
+    )
+
 
   return (
       <>
       <Tooltip id='myid' />
     <div id='heatmap-container' className='container'>
+      {/* {MyResponsiveCalendar(dates)} */}
     <CalendarHeatmap
-        startDate={new Date('2025-01-01')}
-        endDate={new Date('2026-01-01')}
+        startDate={new Date('2024-12-30')}
+        endDate={new Date('2025-12-00')}
         showMonthLabels
-        showWeekdayLabels
+        // showWeekdayLabels
+        values={dates}
+        classForValue={value => {
+          if (!value) {
+            return 'color-empty';
+          }
+          return `color-box-${value.count}`;
+        }}
         gutterSize={3}
         tooltipDataAttrs={(value) => {
           if (!value || !value.date) {
@@ -24,18 +76,8 @@ function Heatmap() {
           return {'data-tooltip-content': value.date, 'data-tooltip-id': 'myid'}
           ;
         }}
-        values={[
-            { date: '2025-01-01', count: 12 },
-            { date: '2025-01-22', count: 122 },
-            { date: '2025-01-30', count: 38 },
-            // ...and so on
-        ]}
         />
-      {/* <div id='heatmap'>
-        {boxes.map((item,index)=>{
-          return <div key={index} className='box'></div>
-        })}
-      </div> */}
+      
     </div>
     </>
   )
